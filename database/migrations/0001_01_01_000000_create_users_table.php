269ec458ec21439c629a9f1bf872e280f6181fbe
John Mark Victorino <victorinojohnmark@gmail.com>
2025-01-19 10:39:29 +0800

    adjustment(User): added user-type on users_table migration file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_no');
+            $table->string('user_type')->default('user'); // user. seller, admin
             $table->rememberToken();
             $table->timestamps();
         });
