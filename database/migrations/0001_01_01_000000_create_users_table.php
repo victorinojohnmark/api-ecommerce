66169902735d91aa3d1c1d1ae17357cc504f14cb
John Mark Victorino <victorinojohnmark@gmail.com>
2025-01-19 10:43:33 +0800

    adjustment(User): remove user-type

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_no');
-            $table->string('user_type')->default('user'); // user. seller, admin
             $table->rememberToken();
             $table->timestamps();
         });
